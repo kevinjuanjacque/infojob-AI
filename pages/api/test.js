be03f1439e25a1f98423d0917638bdb163f156b1
Kevin Juan Jacque Castillo <kjjacquec@falabella.cl>
2023-05-24 22:08:33 -0400

    change redirect

--- pages/api/test.js
+++ pages/api/test.js
@@ -6,7 +6,7 @@ export default async function  handler(req, res) {
   const {code} = req.body
 
   const response = await axios.post(
-    `https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&client_id=a0fa4caafbf84c298b07ce2658489933&client_secret=DH2Sn6ZWB4vywfbjSFWM4chJn2UzdHYpPd%2FAsYaGG3wrMMS7ox&code=${code}&redirect_uri=http://127.0.0.1:3000/`
+    `https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&client_id=a0fa4caafbf84c298b07ce2658489933&client_secret=DH2Sn6ZWB4vywfbjSFWM4chJn2UzdHYpPd%2FAsYaGG3wrMMS7ox&code=${code}&redirect_uri=${process.env.REDIRECT_URI}`
   )
 
   return res.status(200).json({...response.data })
