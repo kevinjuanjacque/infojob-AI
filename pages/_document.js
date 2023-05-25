d63926055ac864084b573f62524a74c7a160da25
Kevin Juan Jacque Castillo <kjjacquec@falabella.cl>
2023-05-24 21:59:08 -0400

    add hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,26 @@
 
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="es">
       <Head >
+
+      <Script id="hotjar" strategy="lazyOnload">
+          {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3506670,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Hind+Vadodara&display=swap" rel="stylesheet" />
+
       </Head>
       <body className="bg-fondo max ">
         <Main />
